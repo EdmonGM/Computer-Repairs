c775a6b71cef4624111e7372f93fea0f7956f0a1
Edmon <ed.moussa4@gmail.com>
2025-04-03 17:43:34 +0300

    Added role requirement

--- ComputerRepairs/DTOs/Account/RegisterDto.cs
+++ ComputerRepairs/DTOs/Account/RegisterDto.cs
@@ -5,11 +5,13 @@ namespace ComputerRepairs.DTOs.Account
     public class RegisterDto
     {
         [Required]
-        public string? Username { get; set; }
+        public string Username { get; set; }
         [Required]
         [EmailAddress]
-        public string? Email { get; set; }
+        public string Email { get; set; }
         [Required]
         public string? Password { get; set; }
+        [Required]
+        public string Role { get; set; }
     }
 }
