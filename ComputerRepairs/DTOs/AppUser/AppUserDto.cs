60e55fbb61a879dc6dfb8efe3eae10672a85ab79
Edmon <ed.moussa4@gmail.com>
2025-04-03 17:43:46 +0300

    Added role

--- ComputerRepairs/DTOs/AppUser/AppUserDto.cs
+++ ComputerRepairs/DTOs/AppUser/AppUserDto.cs
@@ -4,11 +4,12 @@ namespace ComputerRepairs.DTOs.AppUser
 {
     public class AppUserDto
     {
-        public string Id { get; set; }
-        public string UserName { get; set;}
-        public string Email { get; set;}
-        public string Name { get; set;}
+        public required string Id { get; set; }
+        public required string UserName { get; set;}
+        public required string Email { get; set;}
+        public string? Name { get; set;}
         public decimal Salary { get; set;}
-        public List<TicketDto> Tickets { get; set;}
+        public string? Role { get; set;}
+        public List<TicketDto>? Tickets { get; set;}
     }
 }
