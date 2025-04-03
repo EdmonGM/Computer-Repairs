29b3104e8ab36b62cb445b3e71ac5ae16b01fe9e
Edmon <ed.moussa4@gmail.com>
2025-04-03 17:44:22 +0300

    Add role to mapper

--- ComputerRepairs/Mappers/AppUserMapper.cs
+++ ComputerRepairs/Mappers/AppUserMapper.cs
@@ -5,15 +5,16 @@ namespace ComputerRepairs.Mappers
 {
     public static class AppUserMapper
     {
-        public static AppUserDto ToAppUserDto(this AppUser user)
+        public static AppUserDto ToAppUserDto(this AppUser user, string role)
         {
             return new AppUserDto
             {
                 Id = user.Id,
-                UserName = user.UserName,
-                Email = user.Email,
+                UserName = user.UserName!,
+                Email = user.Email!,
                 Name = user.Name,
                 Salary = user.Salary,
+                Role = role,
                 Tickets = user.Tickets.Select(t => t.ToTicketDto()).ToList(),
             };
         }
