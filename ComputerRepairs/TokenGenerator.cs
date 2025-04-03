c20e2fe89a063756e1353b1b43efdefc4dfa93f4
Edmon <ed.moussa4@gmail.com>
2025-04-03 17:45:46 +0300

    Used JwtOptions

--- ComputerRepairs/TokenGenerator.cs
+++ ComputerRepairs/TokenGenerator.cs
@@ -1,70 +1,87 @@
 ﻿using ComputerRepairs.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace ComputerRepairs
 {
-    public class TokenGenerator(IConfiguration config)
+    public class TokenGenerator(UserManager<AppUser> userManager, IConfiguration config)
     {
-        private readonly IConfiguration _config = config;
-        public string GenerateAccessToken(AppUser user)
+        private readonly JwtOptions _jwtOptions =  config.GetSection("Jwt").Get<JwtOptions>() ?? throw new InvalidOperationException("JWT Key is missing in config file");
+        private readonly UserManager<AppUser> _userManager = userManager;
+
+        public async Task<string> GenerateAccessToken(AppUser user)
         {
+            Log.Information("{username} is generating access token", user.UserName);
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions?.SigningKey!));
 
             var claims = new List<Claim>
             {
-                new(JwtRegisteredClaimNames.Sub , user.Id),
-                new(JwtRegisteredClaimNames.UniqueName, user.UserName),
-                new(JwtRegisteredClaimNames.Email, user.Email),
+                new(JwtRegisteredClaimNames.Sub, user.Id),
+                new(JwtRegisteredClaimNames.UniqueName, user.UserName!),
+                new(JwtRegisteredClaimNames.Email, user.Email!),
             };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddMinutes(3),
-                Issuer = _config["JwtSettings:Issuer"],
-                Audience = _config["JwtSettings:Audience"],
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                Issuer = _jwtOptions?.Issuer,
+                Audience = _jwtOptions?.Audience,
                 SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
-        public string GenerateRefreshToken(AppUser user)
+        public async Task<string> GenerateRefreshToken(AppUser user)
         {
+            Log.Information("{username} is generating refresh token", user.UserName);
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions?.SigningKey!));
 
             var claims = new List<Claim>
             {
                 new(JwtRegisteredClaimNames.Sub , user.Id),
-                new(JwtRegisteredClaimNames.UniqueName, user.UserName),
-                new(JwtRegisteredClaimNames.Email, user.Email),
+                new(JwtRegisteredClaimNames.UniqueName, user.UserName!),
+                new(JwtRegisteredClaimNames.Email, user.Email!),
             };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.UtcNow.AddDays(7),
-                Issuer = _config["JwtSettings:Issuer"],
-                Audience = _config["JwtSettings:Audience"],
+                Issuer = _jwtOptions?.Issuer,
+                Audience = _jwtOptions?.Audience,
                 SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
-        public ClaimsPrincipal GetPrincipalFromRefreshCookie(string token)
+        public ClaimsPrincipal? GetPrincipalFromRefreshCookie(string token)
         {
             var tokenValidationParameters = new TokenValidationParameters
             {
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"])),
-                ValidIssuer = _config["JwtSettings:Issuer"],
-                ValidAudience = _config["JwtSettings:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions?.SigningKey!)),
+                ValidIssuer = _jwtOptions?.Issuer,
+                ValidAudience = _jwtOptions?.Audience,
                 ValidateIssuer = true,
                 ValidateAudience = true,
                 ValidateLifetime = true,
@@ -75,7 +92,7 @@ namespace ComputerRepairs
             var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
             if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
             {
-                throw new SecurityTokenException("Invalid Token");
+                return null;
             }
             return principal;
         }
